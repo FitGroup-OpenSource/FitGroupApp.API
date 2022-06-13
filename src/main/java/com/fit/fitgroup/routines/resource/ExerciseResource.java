39439458c672ea279b9578cffa9709db06a4fbf0
price <nicole_14_2015@hotmail.com>
2022-06-13 03:02:35 -0500

    feat: Added Exercise Resource and Save Exercise Resource files

---
+++ src/main/java/com/fit/fitgroup/routines/resource/ExerciseResource.java
@@ -0,0 +1,10 @@
+package com.fit.fitgroup.routines.resource;
+
+import com.fit.fitgroup.shared.domain.model.AuditModel;
+
+public class ExerciseResource extends AuditModel {
+    private Long id;
+    private String name;
+    private String difficulty;
+    private String url;
+}
